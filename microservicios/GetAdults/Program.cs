723ac5289557f3363128bed8dd4403dd814bb764
aminespinoza10 <amin.espinoza@outlook.com>
2025-04-23 22:11:47 -0500

    Variables de ambiante agregadas

--- microservicios/GetAdults/Program.cs
+++ microservicios/GetAdults/Program.cs
@@ -6,9 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                       ?? Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");
+
+
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")) ?? Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");
+    options.UseSqlServer(connectionString);
 });
 
 var app = builder.Build();
